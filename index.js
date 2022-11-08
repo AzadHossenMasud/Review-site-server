adb40733b0834010c87ffa2aaa2d0fce82ac90bf
Azad Hossen <azadhossen6677@gmail.com>
2022-11-08 22:12:08 +0600

    services inserted

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT ||  5000
+require('dotenv').config()
+const cors = require('cors')
+
+// 
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2kitjkk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run= async()=>{
+  try{
+    const serviceCollection = client.db('serviceReview').collection('services')
+
+    app.get('/services', async(req, res)=>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.limit(3).toArray()
+      // console.log(result);
+      res.send(result)
+    })
+    app.get('/allservices', async(req, res)=>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray()
+      // console.log(result);
+      res.send(result)
+    })
+    
+  }
+  finally{
+
+  }
+}
+
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
